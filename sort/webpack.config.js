e7747637920925c6f6d10b5153432e86d78e38d3
linhao <384257538@qq.com>
2019-12-18 16:20:26 +0800

    add webpack-hot

--- sort/webpack.config.js
+++ sort/webpack.config.js
@@ -1,3 +1,5 @@
+const webpack = require("webpack")
+
 module.exports = {
   mode: "development",
   devtool: "inline-source-map",
@@ -5,10 +7,17 @@ module.exports = {
   output: {
     filename: "bundle.js"
   },
+  devServer: { 
+    contentBase: './dist',
+    hot: true
+  },
   resolve: {
     // Add `.ts` and `.tsx` as a resolvable extension.
     extensions: [".ts", ".tsx", ".js"]
   },
+  plugins: [
+     new webpack.HotModuleReplacementPlugin(),
+  ],
   module: {
     rules: [
       // all files with a `.ts` or `.tsx` extension will be handled by `ts-loader`
@@ -20,6 +29,13 @@ module.exports = {
             loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
         }, {
             loader: "sass-loader" // 将 Sass 编译成 CSS
+        }]
+      },
+      { test: /\.css$/,
+        use: [{
+            loader: "style-loader" // 将 JS 字符串生成为 style 节点
+        }, {
+            loader: "css-loader" // 将 CSS 转化成 CommonJS 模块
         }]
       },
     ]
